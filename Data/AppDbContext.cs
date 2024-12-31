195cbf5ef4150bfd0bcd0ebef72fc785733ddcec
code-Elias <eligolam.mahfuzul.dev@gmail.com>
2024-12-31 03:15:41 +0100

    initial commit

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RepositoryNetAPI.Models;
+
+namespace RepositoryNetAPI.Data
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+    {
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,15 @@
+﻿namespace RepositoryNetAPI.Models
+{
+    public class User
+    {
+        public string? Id { get; set; }
+        public string? UserName { get; set; }
+        public string? FirstNames {  get; set; }
+        public string? LastName { get; set;}
+        public string? Alias { get; set; }
+        public int? IsAdmin { get; set; }
+        public int? MainRoleId {  get; set; }
+        public int? IsActive { get; set;}
+        public int? ContactInformationId { get; set; }
+    }
+}
