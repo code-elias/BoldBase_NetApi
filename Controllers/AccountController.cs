161962d89ef60769e4e3b5b31daace65cd08f65a
code-Elias <eligolam.mahfuzul.dev@gmail.com>
2024-12-31 14:42:15 +0100

    Added Account controller

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using RepositoryNetAPI.Models;
+using RepositoryNetAPI.Models.AppModels;
+using RepositoryNetAPI.Repositories.Interfaces;
+
+namespace RepositoryNetAPI.Controllers
+{
+    [Route("api/account")]
+    [ApiController]
+    public class AccountController(IRepository<User> userRepository) : ControllerBase
+    {
+        private readonly IRepository<User> _userRepository = userRepository;
+        private readonly PasswordHasher<User> _passwordHasher = new();
+
+        [HttpPost("register")]
+        public IActionResult Register([FromBody] Account newAccount)
+        {
+            User newUser = newAccount.GetUserModel();
+            newUser.PasswordHash = _passwordHasher.HashPassword(newUser, newUser.PasswordHash);
+
+            _userRepository.Add(newUser);
+            _userRepository.Save();
+
+            return Ok("User registered successfully");
+        }
+    }
+}
---
+++ Models/AppModels/Account.cs
@@ -0,0 +1,25 @@
+﻿namespace RepositoryNetAPI.Models.AppModels
+{
+    public class Account
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string FirstNames { get; set; } = "Test";
+        public string LastName { get; set; } = "Test";
+        public string Email { get; set; } = "test@test.com";
+        public string PhoneNumber { get; set; } = "0123 456789"; 
+
+
+        public User GetUserModel()
+        {
+            return new User()
+            {
+                UserName = UserName,
+                PasswordHash = Password,
+                FirstNames = FirstNames,
+                LastName = LastName,
+                Notes = "Test User"
+            };
+        }
+    }
+}
